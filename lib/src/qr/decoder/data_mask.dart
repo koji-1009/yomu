2bc70bc5963f5b7243d8e39db2d03d0b65ead53d
Koji Wakamiya <koji.wakamiya@gmail.com>
2026-02-19 07:44:44 +0900

    perf: Cache data mask patterns as BitMatrix and apply them via word-level XOR

--- lib/src/qr/decoder/data_mask.dart
+++ lib/src/qr/decoder/data_mask.dart
@@ -21,13 +21,28 @@ enum DataMask {
     binary111 => ((((i + j) & 0x01) + ((i * j) % 3)) & 0x01) == 0,
   };
 
+  // Cache of precomputed mask patterns as BitMatrix for word-level XOR.
+  static final Map<(DataMask, int), BitMatrix> _maskBitmapCache = {};
+
   void unmaskBitMatrix(BitMatrix bits, int dimension) {
-    for (var i = 0; i < dimension; i++) {
-      for (var j = 0; j < dimension; j++) {
-        if (isMasked(i, j)) {
-          bits.flip(j, i);
+    final key = (this, dimension);
+    var maskBits = _maskBitmapCache[key];
+    if (maskBits == null) {
+      maskBits = BitMatrix(width: dimension);
+      for (var i = 0; i < dimension; i++) {
+        for (var j = 0; j < dimension; j++) {
+          if (isMasked(i, j)) {
+            maskBits.set(j, i);
+          }
         }
       }
+      _maskBitmapCache[key] = maskBits;
+    }
+    // Word-level XOR: process 32 bits at a time
+    final src = maskBits.bits;
+    final dst = bits.bits;
+    for (var k = 0; k < dst.length; k++) {
+      dst[k] ^= src[k];
     }
   }
 }
